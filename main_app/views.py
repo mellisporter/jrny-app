0da509bd7d3d460a53f7e785672d276db249f194
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-20 11:40:25 -0400

    home template and url added

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render # allows us to render different views
 from django.http import HttpResponse # allows us to get repsonses to Http Requests
 
 #defines our home view
-def home(reqest):
-    return HttpResponse('<h1>Welcome to JRNY</h1>')
+def home(request):
+    return render(request, 'home.html') # returns our home template instead of plain text
 
 def about(request):
     return render(request, 'about.html') # changed from text to rendering the about template
