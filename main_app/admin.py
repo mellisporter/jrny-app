45c092b95ac146560ba35707dc1fe88b25690f4a
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-20 15:02:31 -0400

    fixed second model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Workout # imports workout model
+from .models import Workout, Exercise # imports workout model
 
 
 # Register your models here.
 admin.site.register(Workout)
+admin.site.register(Exercise)
 
 
 
