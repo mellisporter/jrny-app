b783ccec16e0c7201614f508633e3f22c63ebff4
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-25 10:51:37 -0400

    login path created

--- jrny/urls.py
+++ jrny/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include # allows us to create paths and include in
 urlpatterns= [
     path('admin/' , admin.site.urls), #creates the admin url path for us
     path('', include('main_app.urls')), # creates the home/splash page
+    path('accounts/' , include('django.contrib.auth.urls')), # allow users to be created
 ]
