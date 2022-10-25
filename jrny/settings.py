1fdaef75b334a6035963038eac7d16e0d2740a91
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-25 15:51:00 -0400

    Config for deployment

--- jrny/settings.py
+++ jrny/settings.py
@@ -126,3 +126,7 @@ LOGOUT_REDIRECT_URL = '/' # takes logged out user home instead of admin page
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# heroku config
+import django_heroku
+django_heroku.settings(locals())
