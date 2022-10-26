ca8c21418390580497db8a1d2c5974f8aaa0c987
Matt Porter <matthew.ellis.porter@gmail.com>
2022-10-25 21:27:14 -0400

    config progress

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path # allows us to create url paths
 from . import views #gives our url file access to the views we have defined
 
 urlpatterns= [
-    path('', views.home, name='home'), # our main url path now pulls the home view named home
+    path('/', views.home, name='home'), # our main url path now pulls the home view named home
     path('about/' , views.about, name='about'), # our about path
     path('workouts/', views.workouts_index, name='index'), # index path
     path('workouts/<int:workout_id>/' , views.workouts_detail, name='detail'), # workout detail page
